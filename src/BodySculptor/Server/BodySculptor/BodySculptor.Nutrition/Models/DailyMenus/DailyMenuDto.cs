f2914ae33c9935dbeb0ab00bf37fa18f99afb799
kriskok95 <kriskobota@abv.bg>
2020-07-27 23:19:51 +0300

    Refactoring in DailyMenuDto.cs

--- src/BodySculptor/Server/BodySculptor/BodySculptor.Nutrition/Models/DailyMenus/DailyMenuDto.cs
+++ src/BodySculptor/Server/BodySculptor/BodySculptor.Nutrition/Models/DailyMenus/DailyMenuDto.cs
@@ -1,15 +1,24 @@
 ﻿namespace BodySculptor.Nutrition.Models.DailyMenus
 {
+    using AutoMapper;
     using BodySculptor.Nutrition.Data.Entities;
     using BodySculptor.Services.Mapping;
     using System.Collections.Generic;
 
-    public class DailyMenuDto : IMapFrom<DailyMenu>
+    public class DailyMenuDto : IMapFrom<DailyMenu>, IHaveCustomMappings
     {
         public int Id { get; set; }
+
+        public string Date { get; set; }
 
         public IEnumerable<DailyMenuFoodDto> DailyMenuFoods { get; set; }
 
         public decimal Water { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<DailyMenu, DailyMenuDto>()
+                 .ForMember(x => x.Date, y => y.MapFrom(x => x.Date.ToString("dd-MM yyyy")));
+        }
     }
 }
