5dff0d47eff825fcedac9449ed885c735d7c3c23
kriskok95 <kriskobota@abv.bg>
2020-11-05 20:23:03 +0200

    Update ContainerTests

--- src/Tests/ContainerTests.ps1
+++ src/Tests/ContainerTests.ps1
@@ -3,7 +3,7 @@ do {
     $count++
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
     
-    $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
+    $testStart = Invoke-WebRequest -Uri http://localhost:3000 -UseBasicParsing
     
     if ($testStart.statuscode -eq '200') {
         $started = $true
